e34e22bb45652ecc06b2e4ca63b0bab82e6f35e6
RajavJain <rajavjain2002@gmail.com>
2022-10-17 06:43:40 +0530

    Heading Section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter as Router} from "react-router-dom"
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
 import Offers from "./components/Offers.js"
-
+import Heading from "./components/Heading.js"
 function App() {
   return (
 
@@ -15,6 +15,7 @@ function App() {
           <Navbar/>
           <Slider start={data.banner.start} />
           <Offers offer={data.offer} />
+          <Heading/>
     </Router>
 
   );
